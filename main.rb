2d3012973b307082d6218e8f7891ec4e6ab2b9ee
jingjing <yoohyojin@gmail.com>
2016-06-24 15:25:04 -0400

    main.rb nav layout:false

--- main.rb
+++ main.rb
@@ -3,7 +3,8 @@ require 'sinatra'
 #to load the page is get 'path' do~end is called block and there would be logic of site there
 #get('/') 
  get '/' do    
-   erb :index  #return erb(:index) last line of every block it only that will be display
+   erb :index, layout:false 
+   #return erb(:index) last line of every block it only that will be display
 end
 
  get '/info' do    
