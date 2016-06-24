1c4fe71e100883ee97dfc651289fab04927fa2fd
jingjing <yoohyojin@gmail.com>
2016-06-23 23:33:07 -0400

    CSS for resevation

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	var form_content=$('#form_content');
+	$(cborder).hide();
+	$('#contactBtn').click(function(e){
+		$('#thanks').remove();
+		$(cborder).show();
+	});
+	$('#sendButton').click(function(e){
+		$(cborder).hide();
+		$('#Ccontent').append("<h2 id='thanks'>THANK YOU!<br> I'll GET BACK TO YOU SHORTLY :)<h2>");
+	});
+});
