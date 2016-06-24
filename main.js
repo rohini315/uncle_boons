e5296a68f2c753e80c84ceebf2a0e2358a2844fa
Ro Rudra <rr1960@nyu.edu>
2016-06-24 16:40:05 -0400

    added js

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
 
-// For index.erb page
+	var $overlay = $('<div id="overlay"></div>');
+	var $image = $("<img>");
 
-	var block_nav=$("#block_nav");
-	var index_nav=$("#index_nav");
+	//An image to overlay
+	$overlay.append($image);
+
+	//Add overlay
+	$("body").append($overlay);
+
+  	//click the image and a scaled version of the full size image will appear
+  	$("#photo-gallery a").click( function(event) {
+    event.preventDefault();
+    var imageLocation = $(this).attr("href");
+
+    //update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+
+    //show the overlay
+    $overlay.show();
+  	});
+
+  	$("#overlay").click(function() {
+    $( "#overlay" ).hide();
+ 	 });
 
 	var form_content=$('#form_content');
 	$(cborder).hide();
